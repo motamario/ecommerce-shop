4f1c9f73cceb435ec0f2ec6333f6dd2566f6fcb3
Mario Mota <rosasmario24@gmail.com>
2023-09-18 13:06:55 -0600

    edit route on App.js

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ const App = () => {
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/product/id" element={<ProductDetails />} />
+      <Route path="/product/:id" element={<ProductDetails />} />
     </Routes>
     <Sidebar />
     <Footer />
     </div>
-  )
+  );
 };
 
 export default App;
